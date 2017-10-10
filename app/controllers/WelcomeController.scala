6ea453b3b6ebae77610115d7de8f7df56cd06555
lukegibbins <luke.gibbins@digital.hmrc.gov.uk>
2017-10-10 11:59:30 +0100

    Corrected route to WelcomeController

---
+++ app/controllers/WelcomeController.scala
@@ -0,0 +1,9 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+class WelcomeController extends Controller {
+  def welcome() = Action {
+    Ok(views.html.welcome())
+  }
+}
---
+++ test/controllers/WelcomeControllerSpec.scala
@@ -0,0 +1,38 @@
+package controllers
+
+import org.scalatestplus.play._
+import org.scalatestplus.play.guice.GuiceOneAppPerTest
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{status, _}
+
+class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
+  "WelcomeControllerSpec GET" should {
+
+    "return a successful response" in {
+      val controller = new WelcomeController
+      val result = controller.welcome.apply(FakeRequest())
+      status(result) mustBe OK
+    }
+
+    "return the /welcome url" in {
+      val request = FakeRequest(GET, "/welcome").withHeaders("Host" -> "localhost")
+      val home = route(app,request).get
+      status(home) mustBe OK
+    }
+
+    "return some HTML" in {
+      val controller = new WelcomeController
+      val result = controller.welcome.apply(FakeRequest())
+      contentType(result) mustBe Some("text/html")
+    }
+
+    "say hello and have a title" in {
+      val controller = new WelcomeController
+      val result = controller.welcome.apply(FakeRequest(GET, "/foo"))
+      contentAsString(result) must include ("<h1>Hello!</h1>")
+      contentAsString(result) must include ("<title>Welcome!</title>")
+    }
+
+
+  }
+}
