24b1f68829629e65de5af6eb994fc90157871e70
lukegibbins <luke.gibbins@digital.hmrc.gov.uk>
2017-10-11 12:24:22 +0100

    Added fake tests and mocking

--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -4,12 +4,22 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
 import play.api.test.FakeRequest
 import play.api.test.Helpers.{status, _}
+import services.GreetingService
+
+
+object FakeMorningGreeter extends GreetingService {
+  override def greeting: String = "Good morning!"
+}
+
+object FakeAfternoonGreeter extends GreetingService {
+  override def greeting: String = "Good afternoon!"
+}
 
 class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
   "WelcomeControllerSpec GET" should {
 
     "return a successful response" in {
-      val controller = new WelcomeController
+      val controller = new WelcomeController(FakeMorningGreeter)
       val result = controller.welcome.apply(FakeRequest())
       status(result) mustBe OK
     }
@@ -21,18 +31,25 @@ class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
     }
 
     "return some HTML" in {
-      val controller = new WelcomeController
+      val controller = new WelcomeController(FakeMorningGreeter)
       val result = controller.welcome.apply(FakeRequest())
       contentType(result) mustBe Some("text/html")
     }
 
-    "say hello and have a title" in {
-      val controller = new WelcomeController
+    "say good morning and have a title" in {
+      val controller = new WelcomeController(FakeMorningGreeter)
       val result = controller.welcome.apply(FakeRequest(GET, "/foo"))
-      contentAsString(result) must include ("<h1>Hello!</h1>")
+      contentAsString(result) must include ("<h1>Good morning!</h1>")
       contentAsString(result) must include ("<title>Welcome!</title>")
     }
 
+    "say good afternoon when it's the afternoon and have a title" in {
+      val controller = new WelcomeController(FakeAfternoonGreeter)
+      val result = controller.welcome().apply(FakeRequest(GET, "/foo"))
+      contentAsString(result) must not include ("<h1>Good morning!</h1>")
+      contentAsString(result) must include ("<h1>Good afternoon!</h1>")
+      contentAsString(result) must include ("<title>Welcome!</title>")
+    }
 
   }
 }
